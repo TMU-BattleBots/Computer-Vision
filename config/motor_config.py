7f5fe8780ab12af9dd75da266bc32d11056b2061
Shushmin Hasan <shushmin.hasan@gmail.com>
2026-08-02 22:00:52 -0400

    Format motor PWM configuration

--- config/motor_config.py
+++ config/motor_config.py
@@ -1,1 +1,1 @@
-""" Motor PWM Configuration 1000-2000 PWMSTOP value - 1500Testing values: """STOP_PWM = 1500#Initial Movement Testing ValuesFORWARD_PWM = 1600SEARCH_LEFT_PWM = 1450SEARCH_RIGHT_PWM = 1550# RangeMIN_PWM = 1000MAX_PWM = 2000
+"""Motor PWM ConfigurationPWM range: 1000-2000STOP value: 1500"""STOP_PWM = 1500# Initial movement testing valuesFORWARD_PWM = 1600SEARCH_LEFT_PWM = 1450SEARCH_RIGHT_PWM = 1550# PWM rangeMIN_PWM = 1000MAX_PWM = 2000
